c0125e0a6bcde51e253a4ccb558f3f3027c0e71b
Eleni Skouroupathi <eleni59@hotmail.com>
2015-10-23 11:26:26 +0100

    Finished chapter 13

--- lib/station.rb
+++ lib/station.rb
@@ -1,10 +1,18 @@
-class Station
 
-	attr_reader :name, :zone
 
-	def initialize (name, zone)
-		@name = "Old Street"
-		@zone = 1
-	end
+# class Station
 
-end
+# 	attr_reader :name, :zone
+
+# 	def initialize (name = "Old Street", zone = 1)
+# 		@name = "Old Street"
+# 		@zone = zone
+# 	end
+
+# end
+
+Station = Struct.new(:name, :zone)
+STATIONS = { 
+	old_street: Station.new("Old Street", 1), 
+	aldgate: Station.new("Aldgate", 3)
+}
